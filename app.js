03c9b163a49046a4f1b73ff788fec74259f35df2
Sushant Mohan <sushant.mad@gmail.com>
2022-11-02 16:49:54 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,197 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.use( express.static( "public" ) );
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: false}));
+var sql = require('mssql/msnodesqlv8');
+
+var loggedInUser = ''
+
+var config = {
+    user: 'admin123',
+    password: 'admin123',
+    server: 'LAPTOP-HIVECADV',
+    driver: 'msnodesqlv8',
+    database: 'Courier_Management'
+};
+
+app.get('/home', function (req,res){
+    loggedInUser = ''
+    res.render('login.ejs')
+});
+
+app.post('/home1', function(req,res){
+    var username = req.body.username;
+    var password = req.body.password;
+    sql.connect(config, async function (err) {
+
+        if (err) console.log(err);
+
+        var request = new sql.Request();
+        console.log(username + " "+password)
+        request.query("Select * from [User] where UserId="+"\'"+username+"\';", function (err, recordset) {
+
+            if (err) console.log(err)
+            var result = recordset.recordset.length == 1;
+            console.log(recordset)
+            if (result) {
+                loggedInUser = recordset.recordset[0].UserId;
+                console.log(loggedInUser);
+                console.log(recordset.recordset)
+                res.render('userHome', {userData: recordset.recordset})
+            } else {
+                res.render('error',{data: {'error': 'Invalid Credentials'}})
+            }
+
+
+            // send records as a response
+        });
+    });
+
+});
+
+app.post('/create', function(req, res){
+    res.render('newOrder',{ordersData: []})
+})
+
+app.get('/create', function(req, res){
+    res.render('newOrder',{ordersData: []})
+})
+
+app.post('/createOrder', function(req,res){
+    var details=''
+    sql.connect(config, async function (err) {
+        if (err) console.log(err);
+        var request = new sql.Request();
+        details = req.body;
+        details.orderId = Math.floor(Math.random() * 10000);
+        details.isFragile = (details.isFragile == 'on');
+        var date = new Date().toISOString().replace(/T.*/,'').split('-').join('-')
+        details.date = date
+        var cmd = "select * from [RateList] where ItemCategory="+"\'"+details.itemcategory+"\'";
+        request.query(cmd, function (err, recordset) {
+
+            if (err) console.log(err)
+            //need to inform of success
+            var rate = recordset.recordset[0]['Rate/Kg/100km']
+            var fragility = recordset.recordset[0].FragilityCharges
+            var cost = details.weight*rate;
+            var total = cost * 0.18 + cost;
+            if (details.isFragile){
+                total += fragility;
+            }
+            details.total = total
+            res.render('payment',{details})
+        });
+    });
+
+})
+
+app.post('/orderComplete', function(req,res){
+    sql.connect(config, async function (err) {
+
+        if (err) console.log(err);
+        var details = JSON.parse(req.body.details)
+        console.log("orderComplete")
+        console.log(details)
+        var request = new sql.Request();
+        
+        var cmd = "insert into [Order] values ("
+        + details.orderId +","
+        + "\'"+details.date+"\',"
+        + "\'"+loggedInUser+"\',"
+        + details.weight+","
+        + details.length+","
+        + details.breadth+","
+        + details.height+","
+        + "\'"+details.receiver+"\',"
+        + details.isFragile
+        + ")";
+        console.log(cmd)
+       //paymentid
+        details.paymentId = Math.random().toString(36).slice(2);
+        console.log("PaymentID")
+        console.log(details.paymentId)
+         request.query(cmd, function (err, recordset) {
+        
+             if (err) console.log(err)
+                var result = recordset
+                console.log(result);
+             // send records as a response
+             // res.render('userHome', {userData: recordset.recordset})
+         });
+    });
+
+});
+
+app.post('/view', function(req,res){
+    sql.connect(config, async function (err) {
+
+        if (err) console.log(err);
+
+        var request = new sql.Request();
+        // Select O.OrderId,O.OrderDate,O.SenderId,O.RecipientId,ID.ItemName,ID.ItemCategory,SD.[Total Weight],O.[LengthofItem (ft)],O.[BreadthofItem (ft)],O.[HeightofItem (ft)],SD.IsFragile from [Order] as O inner join [ShipmentDetails] as SD on O.OrderId = SD.OrderId inner join [ItemDetails] as ID on O.OrderId=ID.OrderId
+        //request.query("Select * from [Order] as O inner join [ShipmentDetails] as SD on O.OrderId = SD.OrderId inner join [ItemDetails] as ID on O.OrderId=ID.OrderId and SenderId="+"\'"+loggedInUser+"\'", function (err, recordset) {
+        request.query("Select O.OrderId,O.OrderDate,O.SenderId,O.RecipientId,ID.ItemName,ID.ItemCategory,O.[WeightofItem (in Kg)],O.[LengthofItem (ft)],O.[BreadthofItem (ft)],O.[HeightofItem (ft)],SD.IsFragile from [Order] as O inner join [ShipmentDetails] as SD on O.OrderId = SD.OrderId inner join [ItemDetails] as ID on O.OrderId=ID.OrderId and SenderId="+"\'"+loggedInUser+"\'", function (err, recordset) {
+            if (err) console.log(err)
+            var result = '';
+            console.log(recordset.recordset);
+
+            // send records as a response
+            res.render('orders', {ordersData: recordset.recordset})
+
+        });
+    });
+});
+
+app.get('/order/modify/:orderId', function (req,res){
+    sql.connect(config, async function (err) {
+
+        if (err) console.log(err);
+
+        var request = new sql.Request();
+
+        request.query("Select * from [Order] as O inner join [ShipmentDetails] as SD on O.OrderId = SD.OrderId inner join [ItemDetails] as ID on O.OrderId=ID.OrderId and O.OrderId="+"\'"+req.params.orderId+"\'", function (err, recordset) {
+
+            if (err) console.log(err)
+            var result = '';
+            console.log(recordset.recordset);
+
+            // send records as a response
+            res.render('newOrder', {ordersData: recordset.recordset})
+
+        });
+    });
+});
+
+app.get('/order/cancel/:orderId', function (req,res){
+    sql.connect(config, async function (err) {
+
+        if (err) console.log(err);
+
+        var request = new sql.Request();
+
+        request.query("Select * from [Order] as O inner join [ShipmentDetails] as SD on O.OrderId = SD.OrderId inner join [ItemDetails] as ID on O.OrderId=ID.OrderId and O.SenderId="+"\'"+loggedInUser+"\'", function (err, recordset) {
+
+            if (err) console.log(err)
+            var result = '';
+            console.log(recordset.recordset);
+
+            // send records as a response
+            res.render('orders', {ordersData: recordset.recordset})
+
+        });
+    });
+});
+
+
+
+var server = app.listen(5000, function () {
+    console.log('Server is running..');
+});
